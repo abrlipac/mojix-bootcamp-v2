2cf7e2edc66314d77b4f83fc15621f098af173f6
Abraham Lipa Calabilla <71676827+abrlipac@users.noreply.github.com>
2022-08-11 19:54:40 -0500

    Fix first example

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -8,7 +8,18 @@ st.markdown('## 1. Walrus operator')
 number1 = st.number_input('Enter a number')
 
 list1 = range[1, number1]
-if(l := len(list1) > 2)
+l1 := len(list1)
+
+st.markdown('### Code example')
+
+st.markdown(f'''```python
+list1 = range{list1}
+print(l1 := len(list1))
+print(l1)```''')
 
 st.markdown('### Output')
-st.markdown(f'```{l}```')
+
+# no se puede imprimir una expresión con este operador
+st.markdown(f'''```
+{l1}
+{l1}```''')
