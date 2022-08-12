1730e806f5c439e5a6d96d1a3f98638d88ec2fc6
Abraham Lipa Calabilla <71676827+abrlipac@users.noreply.github.com>
2022-08-11 19:33:55 -0500

    Add first example

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.write('Hola mundo')
+st.markdown('# 10 Cool Beginner Python Tricks')
+st.markdown('from **Mojix Bootcamp** v2')
+
+st.markdown('## 1. Walrus operator')
+
+number1 = st.number_input('Enter a number')
+
+list1 = range[1, number1]
+if(l := len(list1) > 2)
+
+st.markdown('### Output')
+st.markdown(f'```{l}```')
