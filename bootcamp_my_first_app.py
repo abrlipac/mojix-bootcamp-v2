aab7cc9d966817556db44ea6d17a5e0eefc46ad7
Abraham Lipa Calabilla <abrlipac@upt.pe>
2022-08-14 00:31:37 -0500

    add css file and refactor showing button

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,22 +1,13 @@
 import streamlit as st
 
-st.markdown('''
-  <style>
-  [href="#10-trucos-geniales-de-python-para-principiantes-que-te-facilitar-n-la-vida"] {
-    display: inline-block;
-    outline: none;
-    cursor: pointer;
-    font-size: 16px;
-    line-height: 20px;
-    font-weight: 600;
-    border-radius: 8px;
-    padding: 14px 24px;
-    border: none;
-    transition: box-shadow 0.2s ease 0s, -ms-transform 0.1s ease 0s, -webkit-transform 0.1s ease 0s, transform 0.1s ease 0s;
-    background: linear-gradient(to right, rgb(230, 30, 77) 0%, rgb(227, 28, 95) 50%, rgb(215, 4, 102) 100%);
-    color: #fff;
-  }
-</style>''', unsafe_allow_html=True)
+with open("style.css") as f:
+    st.markdown(
+        f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+def show_button():
+    st.markdown('<a href="#10-trucos-geniales-de-python-para-principiantes-que-te-facilitar-n-la-vida" class="go-top">Regresar arriba ☝️</a>', unsafe_allow_html=True)
+
 
 st.title("10 trucos geniales de Python para principiantes que te facilitarán la vida")
 
@@ -56,7 +47,7 @@ st.subheader("Salida")
 
 st.code('3')
 
-st.markdown("[Regresar arriba ☝️](#10-trucos-geniales-de-python-para-principiantes-que-te-facilitar-n-la-vida)")
+show_button()
 
 st.header("2. Dividir una cadena")
 
@@ -72,7 +63,7 @@ st.subheader("Salida")
 
 st.code("['hello', 'world']")
 
-st.markdown("[Regresar arriba ☝️](#10-trucos-geniales-de-python-para-principiantes-que-te-facilitar-n-la-vida)")
+show_button()
 
 st.header("3. Revertir una cadena")
 
@@ -89,7 +80,7 @@ st.subheader("Salida")
 
 st.code("!dlrow olleh")
 
-st.markdown("[Regresar arriba ☝️](#10-trucos-geniales-de-python-para-principiantes-que-te-facilitar-n-la-vida)")
+show_button()
 
 st.header("4. Fusionando dos diccionarios")
 
@@ -107,7 +98,7 @@ st.subheader("Salida")
 
 st.code("{'a': 10, 'b': 20, 'c': 30, 'd': 40}")
 
-st.markdown("[Regresar arriba ☝️](#10-trucos-geniales-de-python-para-principiantes-que-te-facilitar-n-la-vida)")
+show_button()
 
 st.header("5. La función zip()")
 
@@ -142,7 +133,7 @@ st.subheader("Salida 2")
 
 st.code("{'Rajesh': 87, 'kumar': 90, 'Kriti': 88}")
 
-st.markdown("[Regresar arriba ☝️](#10-trucos-geniales-de-python-para-principiantes-que-te-facilitar-n-la-vida)")
+show_button()
 
 st.header("6. Asignación de múltiples valores de lista a una variable")
 
@@ -164,7 +155,7 @@ b = [2, 3, 4, 5]''')
 
 st.markdown("Este proceso también se llama desempaquetado de la lista y puede aplicar este método para más de 2 variables también.")
 
-st.markdown("[Regresar arriba ☝️](#10-trucos-geniales-de-python-para-principiantes-que-te-facilitar-n-la-vida)")
+show_button()
 
 st.header("7. Eliminar elementos de la lista duplicada")
 
@@ -181,7 +172,7 @@ st.subheader("Salida")
 
 st.code("{1, 2, 3, 4, 5, 6, 7, 8, 9}")
 
-st.markdown("[Regresar arriba ☝️](#10-trucos-geniales-de-python-para-principiantes-que-te-facilitar-n-la-vida)")
+show_button()
 
 st.header("8. Función lambda")
 
@@ -199,7 +190,7 @@ st.subheader("Salida")
 
 st.code("30")
 
-st.markdown("[Regresar arriba ☝️](#10-trucos-geniales-de-python-para-principiantes-que-te-facilitar-n-la-vida)")
+show_button()
 
 st.header("9. Intercambio de valor de variable")
 
@@ -213,14 +204,14 @@ b = 200
 a,b = b,a
 print(f'a = ',a)
 print(f'b = ',b)''')
-  
+
 st.subheader("Salida")
 
 st.code('''
 a = 200
 b = 100''')
 
-st.markdown("[Regresar arriba ☝️](#10-trucos-geniales-de-python-para-principiantes-que-te-facilitar-n-la-vida)")
+show_button()
 
 st.header("10. Use una contraseña en su código")
 
@@ -245,6 +236,8 @@ password: **** [abcd]
 Welcome stranger!
 Password: **** [abdc]
 Wrong password''')
+
+show_button()
 
 st.markdown('''
 > No olvides ver el artículo original: [10 Cool Beginner Python Tricks That Will Make Your Life Easier](https://levelup.gitconnected.com/10-cool-beginner-python-tricks-that-will-make-your-life-easier-6c68c909edf6)''')
